8ecd8f24ca4b84e14139b71bb6cc34ff3142683a
mohag123 <cksgus5336@gmail.com>
2021-09-17 21:48:59 +0900

    crawler update

--- backend/yt-music.py
+++ backend/yt-music.py
@@ -16,12 +16,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 import django
 django.setup()
 
-import sys
-from os import path
 if __name__ == '__main__':
-    if __package__ is None:
-        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-        from songAPI.models import Song
+    from songAPI.models import Song, Profile
         
 # Youtube music login
 driver = webdriver.Chrome('chromedriver.exe')
@@ -42,7 +38,22 @@ while True:
             driver.switch_to.window(driver.window_handles[0])
 
 while True:
-    if ("browse" in driver.current_url):
+    if "browse" in driver.current_url:
+        # Select singer
+        data = input('\nSongs will be scraped.\nIf you want to escape, input "quit".\nInput singer_id : ')
+        if(data == "quit"):
+            driver.get(YOUTUBE_MUSIC)
+            continue
+        try:
+            singer_id = Profile.objects.get(pk=data)
+        except Profile.DoesNotExist:
+            print("\n[!] There's no profile data.\n[!] You must select another singer_id.")
+            print("\n[!] If you want to escape, input 'quit'")
+            continue
+        except ValueError:
+            print("\n[!] Input value must be number type.")
+            continue
+
         # Infinite Scroll
         driver.implicitly_wait(2)
         element = driver.find_element_by_tag_name('body')
@@ -50,39 +61,49 @@ while True:
             element.send_keys(Keys.END)
             time.sleep(1)
 
-        # Scraping data
+        # Scraping Data
         base_url = 'https://youtube.com/'
         soup = BeautifulSoup(driver.page_source, 'lxml')
 
-        singer = soup.select_one('div.secondary-flex-columns > yt-formatted-string > a.yt-simple-endpoint').text
+        lists_secondary = soup.select('div.secondary-flex-columns > yt-formatted-string > a.yt-simple-endpoint')
         lists_title = soup.select('div.title-column > yt-formatted-string > a.yt-simple-endpoint')
         imgtags = soup.select('ytmusic-thumbnail-renderer > yt-img-shadow#image > img#img')
 
         images = []
         for img in imgtags:
             images.append(img['src'])
-
-        datalist = []
         for i in range(len(lists_title)):
             link = lists_title[i]['href']
             if(link is not None):
                 link = link[:link.index("&")]
+                singer = lists_secondary[i*2].text
                 title = lists_title[i].text
-                image = images[i].replace("w60", "w544").replace("h60", "h544")
-                datalist.append({
-                    "link" : base_url + link,
-                    "singer" : singer,
-                    "title" : title,
-                    "image" : image
-                })
-
-        # Saving data
-        for d in datalist:
-            Song.objects.create(
-                link = d['link'],
-                singer = d['singer'],
-                title = d['title'],
-                image = d['image']
+                image = images[i].replace("w60", "w544").replace("h60", "h544")                
+                Song.objects.create(
+                    link = base_url + link,
+                    singer = singer,
+                    title = title,
+                    image = image,
+                    singer_id = singer_id
             )
         driver.get(YOUTUBE_MUSIC)
         print("\nData saved.\n")
+    elif "channel" in driver.current_url:
+        data = input('\nDoes scrap this profile image?\nInput any key to proceed: ')
+        if("channel" not in driver.current_url):
+            print("\n[!] you are off the page.")
+        else:
+            print("\nScraping profile image starts.")
+            # scraping data
+            soup = BeautifulSoup(driver.page_source, 'lxml')
+            singer = soup.select_one('div.content-container > yt-formatted-string.title').text
+            imgtag = soup.select_one('ytmusic-fullbleed-thumbnail-renderer > picture > source')
+
+            # saving data
+            Profile.objects.create(
+                singer = singer,
+                profile = imgtag['srcset'].replace('w540', 'w1920').replace('h225', 'h800')
+            )
+
+            driver.get(YOUTUBE_MUSIC)
+            print("\nData saved.\n")
