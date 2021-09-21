2f43af199228fd029a6802c56f216448ea1b2c0b
mohag123 <cksgus5336@gmail.com>
2021-09-21 18:21:04 +0900

    code line modified

--- src/Write.js
+++ src/Write.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { InputGroup,FormControl, Button } from "react-bootstrap";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
 
 export default function WritePage(){
     return(
@@ -8,27 +8,27 @@ export default function WritePage(){
             <InputGroup className="mb-3">
                 <InputGroup.Text id="title">제목</InputGroup.Text>
                 <FormControl
-                aria-label="제목"
-                aria-describedby="inputGroup-sizing-default"
+                    aria-label="제목"
+                    aria-describedby="inputGroup-sizing-default"
                 />
             </InputGroup>
             <InputGroup className="mb-3">
                 <InputGroup.Text id="writer">작성자</InputGroup.Text>
                 <FormControl
-                aria-label="writer"
-                aria-describedby="inputGroup-sizing-default"
+                    aria-label="writer"
+                    aria-describedby="inputGroup-sizing-default"
                 />
             </InputGroup>
-
             <InputGroup>
                 <InputGroup.Text id ="content">내용</InputGroup.Text>
                 <FormControl as="textarea" aria-label="내용" />
             </InputGroup>
         </div>
         <Button variant="secondary" className="mt-8 mr-5"
-         onclick={()=>{
+            onclick={()=>{
                 // 여기에다가 값보내는 메소드 적으면 댐 axios.put
-        }}>저장</Button>
+            }}>저장
+        </Button>
         </>
     )
 }
