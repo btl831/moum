a6214f13c2f6d78e0d236415657337e2b4cd1a6c
mohag123 <cksgus5336@gmail.com>
2021-09-27 00:03:29 +0900

    파일 정리

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './google/serviceWorkerRegistration';
+import reportWebVitals from './google/reportWebVitals';
 
 import {BrowserRouter} from 'react-router-dom';
 
