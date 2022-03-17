c14e2aac790bae1381f23467d91ee6e81c4464c4
신봉규 <daoh98@naver.com>
2022-03-17 11:36:20 +0900

    프로젝트 재 복제 및 README.md 수정

--- backend/backend/urls.py
+++
@@ -1,9 +0,0 @@
-# /backend/backend/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/song/', include('songAPI.urls')),
-]
