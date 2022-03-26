f1ea98487c67da9f8609b5cf5601c28fe024944e
mohag123 <cksgus5336@gmail.com>
2022-03-27 00:19:09 +0900

    ListPage

--- src/community/ListPage.js
+++ src/community/ListPage.js
@@ -5,6 +5,7 @@ import './ListPage.module.css';
 
 export default function ListPage() {
     const [text,setText] = useState([]);
+
     useEffect(()=>{
         db.collection('Comment').get().then((result)=>{
             var array =[];
@@ -14,36 +15,29 @@ export default function ListPage() {
             
             setText(array);
         })
-    });
-    
+    });    
+
     return(
         <>
-        <div class="container mt-3">
+        <div className="full container mt-3">
             <ListGroup as="ol" numbered>
-        {
-            text.map((a,i)=>{
-                return(
-                    <>
-                        
-                        <ListGroup.Item
-                            as="li"
-                            className="d-flex justify-content-between align-items-start"
-                            
-                        >
-                            <div className="ms-2 me-auto">
-                            <div className="fw-bold" onClick={()=>{window.location.href = "/detail/{a.id}"}}
-                            >{i}.{a.title}</div>
+            {
+                text.map((a,i)=>
+                    <ListGroup.Item
+                        as="li"
+                        className="d-flex justify-content-between align-items-start"
+                        key = {i}
+                    >
+                        <div className="ms-2 me-auto">
+                            <a className="fw-bold" href={'/detail/' + a.id} style={{color:"black"}}>
+                                {i}.{a.title}
+                            </a><br/>
                             {a.context}
-                            </div>
-                            <Badge bg="primary" pill>
-                            14
-                            </Badge>
-                        </ListGroup.Item>
-                        
-                    </>
+                        </div>
+                        <Badge bg="primary" pill> 14 </Badge>
+                    </ListGroup.Item>
                 )
-            })
-        }
+            }
             </ListGroup>
         </div>
         </>
