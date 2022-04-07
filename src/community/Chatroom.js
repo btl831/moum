c07bace532196f6599a629e43f0291d2ee560846
신봉규 <coodori@sinbong-gyuui-MacBookAir.local>
2022-04-07 23:31:33 +0900

    채팅방 목록 구성완료

--- src/community/Chatroom.js
+++ src/community/Chatroom.js
@@ -1,53 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import "./Chatroom.module.css";
+import React, { Component, useEffect, useState } from 'react';
+import "./Chatroom.css";
 import { db } from '../firebase/fBase';
 
 export default function Chatroom() {
     var myuid = JSON.parse(localStorage.getItem('user')).uid;
-    var chatlst = [];
+    var chatlst;
+    var [chat,setchat] = useState([]);
     useEffect(() => {
         db.collection('chatroom').where('who', 'array-contains', myuid).get().then((result) => {
             var array = [];
             result.forEach((doc) => {
-                array.push(doc.data());
-                
+                array.push(doc.data(),doc.id);
             });
             console.log(array);
-            chatlst =array;
+            chatlst = array;
+            setchat(array);
             console.log(chatlst);
+            console.log(chat);
         })
     }, []);
 
     return (
         <>
-            <div class="container p-4 detail">
-                <div class="row">
-                    <div class="col-3 p-0">
-                        <ul class="list-group chat-list">
-
-                            {
-                                chatlst && chatlst.map((a,i)=> 
-                                            
-                                            <li class="list-group-item" key = {i}>
-                                                <h6>{a.title}</h6>
-                                                <h6 class="text-small">{a.date}</h6>
-                                            </li>
-                                                    
-                                )
+            <div className="container p-4 detail">
+                <div className="row">
+                    <div className="col-3 p-0">
+                        <ul className="list-group chat-list">
+                            {   
+                                chat.map((a,i)=>{
+                                    return <Chatlist title ={a.title} date = {a.id}></Chatlist>
+                                })
                             }
                             
+
                         </ul>
                     </div>
-                    <div class="col-9 p-0">
-                        <div class="chat-room">
-                            <ul class="list-group chat-content">
-                                <li><span class="chat-box">채팅방1 내용</span></li>
-                                <li><span class="chat-box">채팅방1 내용</span></li>
-                                <li><span class="chat-box mine">채팅방1 내용</span></li>
+                    <div className="col-9 p-0">
+                        <div className="chat-room">
+                            <ul className="list-group chat-content">
+                                <li><span className="chat-box">채팅방1 내용</span></li>
+                                <li><span className="chat-box">채팅방1 내용</span></li>
+                                <li><span className="chat-box mine">채팅방1 내용</span></li>
                             </ul>
-                            <div class="input-group">
-                                <input class="form-control" id="chat-input" />
-                                <button class="btn btn-secondary" id="send">전송</button>
+                            <div className="input-group">
+                                <input className="form-control" id="chat-input" />
+                                <button className="btn btn-secondary" id="send">전송</button>
                             </div>
                         </div>
                     </div>
@@ -55,4 +52,15 @@ export default function Chatroom() {
             </div>
         </>
     )
+}
+
+class Chatlist extends Component{
+    render(){
+        return (
+            <li className="list-group-item">
+                                <h6>{this.props.title}</h6>
+                                <h6 className="text-small">{this.props.date}</h6>
+            </li> 
+        )
+    }
 }
