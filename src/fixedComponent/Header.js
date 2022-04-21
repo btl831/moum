d172a3a29195ba186a44c80d19381c4b7b5ee1d5
mohag123 <cksgus5336@gmail.com>
2022-04-21 16:07:45 +0900

    CSS changed

--- src/fixedComponent/Header.js
+++ src/fixedComponent/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
-import './Fixed.css'
+import styles from './Header.module.css'
 
 export default function Header() {
     var userObj = JSON.parse(localStorage.getItem('user'));
@@ -10,30 +10,29 @@ export default function Header() {
     [userObj]);
 
     return(
-        <div className="sticky">
+        <div className={styles.sticky}>
         <Container>
             <Navbar bg="dark" variant="dark" style={{ height:"62px" }}>
                 <Navbar.Brand href="/" >
-                <img
-                    alt="MOUM"
-                    src="/logo.png"
-                    height="50px"
-                    className="d-inline-block align-top"
-                />
+                    <img
+                        alt="MOUM"
+                        src="/logo.png"
+                        height="50px"
+                        className="d-inline-block align-top"
+                    />
                 </Navbar.Brand>
                 <Nav className="me-auto">
-                <Nav.Link href="/chart">TOP 100</Nav.Link>
-                <Nav.Link href="/write">Write</Nav.Link>
-                {
-                    userObj != null
-                    ? ( <>
-                    <Nav.Link href="/chatroom">채팅방</Nav.Link>
-                    <Nav.Link href = "/game/wordle/">Wordle</Nav.Link>
-                    </>
-                    )
-                    : null
-                }
-                
+                    <Nav.Link href="/chart">TOP 100</Nav.Link>
+                    <Nav.Link href="/write">Write</Nav.Link>
+                    {
+                        userObj != null
+                        ? ( <>
+                        <Nav.Link href="/chatroom">채팅방</Nav.Link>
+                        <Nav.Link href = "/game/wordle/">Wordle</Nav.Link>
+                        </>
+                        )
+                        : null
+                    }
                 </Nav>
                 {
                     userObj != null
