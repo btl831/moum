9ad5e0ae6ef0b439942774151c5f480e87fa0d35
신봉규 <coodori@sinbong-gyuui-MacBookAir.local>
2022-04-23 17:03:25 +0900

    가수별 playlist 수정

--- src/music/etc/Recommend.js
+++ src/music/etc/Recommend.js
@@ -1,21 +1,65 @@
-import { Figure } from 'react-bootstrap';
-import AlertSong from '../features/AlertSong'
+import './Recommend.css';
+import { Button, Collapse } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { db } from 'firebase/fBase';
+
 
 export default function Recommend() {
+    const [open, setOpen] = useState(false);
+    const [item, setItem] = useState([]);
+    let [click, setClick] = useState();
+
+    useEffect(() => {
+        db.collection('Recommand').get().then((result) => {
+            var array = [];
+            result.forEach((doc) => {
+                array.push({ ...doc.data(), id: doc.id });
+            });
+            console.log(array)
+            setItem(array);
+            setClick()
+        })
+    }, []);
+
     return (
-        <Figure className="mt-3">
-            <Figure.Image
-                width={180}
-                height={180}
-                alt="180x180"
-                src="https://i.ytimg.com/vi/tHmc2mAXZSA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDLny-RWs_I2o50OjZfWAA1vyNHRg"
-                
-                // 미구현
-                onClick = {()=>{<AlertSong />}}
-            />
-            <Figure.Caption>
-                [Playlist] J-POP 입문은 이 밴드로! 2021 요루시카 노래 모음 Yorushika songs ヨルシカ (15곡)
-            </Figure.Caption>
-        </Figure>
+        <>
+            <div className='carousel'>
+                <div className='container pt-5'>
+                    <iframe className='pt-1  pl-3' width="400vh" height="270vh" src="https://www.youtube.com/embed/tHmc2mAXZSA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div className='container row'>
+                {
+                    item.map((a, i) => {
+                        return (
+                            <>
+                                <div className='col-1'></div>
+                                <Button className='btn1 col-3' onClick={() => { setOpen(false); setOpen(!open); setClick(a); }}
+                                    aria-controls="example-collapse-text"
+                                    aria-expanded={open}>{a.singer}</Button>
+                            </>
+                        )
+                    })
+                }
+
+                {/* <div className='col-1'></div>
+
+                <Button className='btn2 col-3'>유이카</Button>
+                <div className='col-1'></div>
+                <Button className='btn3 col-3'>요네즈 켄시</Button> */}
+                <div>
+                    {open == true
+                        ? <Collapse in={open}>
+                            <div id="example-collapse-text">
+                                {click.context}
+                            </div>
+                        </Collapse>
+                        : null
+                    }
+
+                </div>
+            </div>
+        </>
+
     );
 }
