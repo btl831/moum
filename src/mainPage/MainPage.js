9441efe99110af4591658660a018d5d6e4abb46f
mohag123 <cksgus5336@gmail.com>
2022-04-25 11:39:28 +0900

    대문 페이지 버튼 수정

--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -15,7 +15,8 @@ export default function MainPage() {
                 <div className="col-sm-5 col-md-3">
                     <div className={styles.boxContainer}>
                         <p className={styles.text}>
-                            &nbsp;&nbsp;음악페이지로!
+                            &nbsp;&nbsp;&nbsp;
+                            음악페이지
                         </p>
                         <button className={styles.btn} onClick={()=>window.location.href="/music"}>
                             <img src="mainPage/music.png" alt="" className={styles.image} width="260"/>
@@ -24,9 +25,12 @@ export default function MainPage() {
                 </div>
                 <div className="col-sm-5 col-md-3">
                     <div className={styles.boxContainer}>
-                        <p className={styles.text}>&nbsp;&nbsp;게임페이지로!</p>
+                        <p className={styles.text}>
+                            &nbsp;&nbsp;&nbsp;
+                            게임페이지
+                        </p>
                         <button className={styles.btn} onClick={()=>window.location.href="/game"}>
-                            <img src="mainPage/game.jpg" alt="" className={styles.image} height="200"/>
+                            <img src="mainPage/game.jpg" alt="" className={styles.image} height="200" style={{right:"70px"}}/>
                         </button>
                     </div>
                 </div>
