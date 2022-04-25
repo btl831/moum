267ce93248e79311c3660acf8cb40691b5fb363e
mohag123 <cksgus5336@gmail.com>
2022-04-25 14:20:35 +0900

    Login Redirect

--- src/game/GamePage.js
+++ src/game/GamePage.js
@@ -7,7 +7,18 @@ export default function GamePage() {
         <h1>임시 게임 페이지입니다.</h1>
         <br/><br/><br/>
         <div>
-            <Button onClick={()=>window.location.href='/game/wordle'}>Worlde</Button>
+            <Button onClick={() => {
+                var userObj = JSON.parse(localStorage.getItem('user'));
+                if(userObj) {
+                    window.location.href='/game/wordle'
+                }
+                else {
+                    alert("로그인이 필요한 작업입니다.");
+                    window.location.href='/Login'
+                }
+            }}>
+                Worlde
+            </Button>
         </div>
         </>
     );
