c90c6cc96efcaf982340ebea17226ebee4c8ebac
mohag123 <cksgus5336@gmail.com>
2022-05-02 00:08:03 +0900

    fix CSS of musicpage

--- src/fixedComponent/Header.js
+++ src/fixedComponent/Header.js
@@ -9,37 +9,83 @@ export default function Header() {
 
     if(window.location.pathname === "/") return null;
     return(
-        <div className={styles.static}>
-        <Container>
-            <Navbar bg="dark" variant="dark" style={{ height:"62px" }}>
-                <Navbar.Brand href="/" >
-                    <img
-                        alt="MOUM"
-                        src="/logo.png"
-                        height="50px"
-                        className="d-inline-block align-top"
-                    />
-                </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/music">Music</Nav.Link>
-                    <Nav.Link href="/game">Game</Nav.Link>
-                    {
-                        userObj != null
-                        ? ( <>
-                        <Nav.Link href="/music/chatroom">채팅방</Nav.Link>
-                        </>
-                        )
-                        : null
-                    }
-                </Nav>
-                {
-                    userObj != null
-                    ? (<div className= "text-white">{userObj.displayName}님 환영합니다!   <Button onClick={() => { localStorage.removeItem('user'); navigate(0);}}>로그아웃</Button> </div> )
-                    :<Button className='primary' href={"/login"}>로그인</Button>
-                }
-            </Navbar>
-        </Container>
-        </div>
+        <>
+            <div className={styles.static}>
+                <div className="row">
+                    <Navbar bg="dark" variant="dark" style={{ height:"70px" }}>
+                        <div className="col-md-1" />
+                        <div className="col-md-3">
+                            <div style={{float:"left"}}>
+                                <Navbar.Brand href="/" >
+                                    <img
+                                        alt="MOUM"
+                                        src="/logo.png"
+                                        height="50px"
+                                        className="d-inline-block align-top"
+                                    />
+                                </Navbar.Brand>
+                            </div>
+                            <div style={{marginTop:"4px"}}>
+                                <Nav className="me-auto">
+                                    <Nav.Link href="/music">Music</Nav.Link>
+                                    <Nav.Link href="/game">Game</Nav.Link>
+                                    {
+                                        userObj != null
+                                        ? <Nav.Link href="/music/chatroom">채팅방</Nav.Link>
+                                        : null
+                                    }
+                                </Nav>
+                            </div>
+                        </div>
+                        <div className="col-md-4" />
+                        <div className="col-md-3">
+                            <div style={{float:"right"}}>
+                                {
+                                    userObj != null
+                                    ?<div className= "text-white">{userObj.displayName}님 환영합니다!   <Button onClick={() => {localStorage.removeItem('user'); navigate(0);}}>로그아웃</Button></div>
+                                    :<Button className='primary' href={"/login"}>로그인</Button>
+                                }
+                            </div>
+                        </div>
+                        <div className="col-md-1" />
+                    </Navbar>
+                </div>
+            </div>
+
+            {/*
+            <div className={styles.fixed}>
+                <div className="row">
+                    <Navbar style={{ height:"70px" }}>
+                        <div className="col-md-1" />
+                        <div className="col-md-3">
+                            <div style={{float:"left"}}>
+                                <Navbar.Brand href="/" >
+                                    <img
+                                        alt="MOUM"
+                                        src="/logo.png"
+                                        height="50px"
+                                        className="d-inline-block align-top"
+                                    />
+                                </Navbar.Brand>
+                            </div>
+                            <div style={{marginTop:"4px"}}>
+                                <Nav className="me-auto">
+                                    <Nav.Link href="/music">Music</Nav.Link>
+                                    <Nav.Link href="/game">Game</Nav.Link>
+                                    {
+                                        userObj != null
+                                        ? <Nav.Link href="/music/chatroom">채팅방</Nav.Link>
+                                        : null
+                                    }
+                                </Nav>
+                            </div>
+                        </div>
+                        <div className="col-md-8" />
+                    </Navbar>
+                </div>
+            </div>
+            */}
+        </>
     )
 }
 
--- src/music/MusicPage.js
+++ src/music/MusicPage.js
@@ -14,89 +14,84 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function MusicPage(props) {
     let [clickvalue,setClickvalue] = useState("");
     
-
     return(
         <>
         <Container>
-            {/* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ BODY ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */}
             <div className="row" style={{height:"100px"}} />
             <div className="row">
                 <Slider chart_info={props.chart_info} />
             </div>
             <div className="row" style={{height:"200px"}} />
 
-            {/* 가수별 Playlist */}
             <div className="row" style={{position:"relative"}}>
                 <div className={styles.space} />
-                <div className="row" style={{height:"100px"}} />
+                <div className="row" style={{height:"85px"}} />
                 <div className="row" id={styles.wrapper}>
-
-                <div className="col-md-5" id={styles.index}>
-                    <div className='pt-4 margin-left-auto'>
-                    <hr />
-                    <h5>가수별 Playlist</h5>
-                    <FontAwesomeIcon icon={faHeadphones} size={"2x"} className="fa"/>
-                    <hr />
-                    </div>
-                    <div className="pt-2">
-                        <Recommend />
-                    </div>
-                </div>
-
-                {/* TOP 100 */}
-                <div className="col-md-5 mt-5" id={styles.index}>
-                    <div className="row mt-4">
-                        <div className="col-sm-10 col-md-10">
-                            <h5 className={styles.top100}>TOP 100 </h5> 
+                    {/* 가수별 Playlist */}
+                    <div className="col-md-5" id={styles.index}>
+                        <div className='pt-4 margin-left-auto'>
+                            <hr />
+                            <h5>가수별 Playlist</h5>
+                            <FontAwesomeIcon icon={faHeadphones} size={"2x"} className="fa"/>
+                            <hr />
                         </div>
-                        <div className="col-sm-2 col-md-2">
-                            <button className={styles.btn} onClick={()=>window.location.href="/music/chart"}>
-                                <h2>&gt;</h2>
-                            </button>
+                        <div>
+                            <Recommend />
                         </div>
                     </div>
-                    <div className="row mt-2">
-                    <ChartPage chart_info={props.chart_info.slice(undefined, 7)} />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    {/* TOP 100 */}
+                    <div className="col-md-5 mt-1" id={styles.index}>
+                        <div className="row mt-4">
+                            <div className="col-sm-10 col-md-10">
+                                <h5 className={styles.top100}>TOP 100 </h5> 
+                            </div>
+                            <div className="col-sm-2 col-md-2">
+                                <button className={styles.btn} onClick={()=>window.location.href="/music/chart"}>
+                                    <h2>&gt;</h2>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <ChartPage chart_info={props.chart_info.slice(undefined, 7)} />
+                        </div>
+                        <div className="row" style={{ height:"100px"}} />
                     </div>
-                    <div className="row" style={{ height:"100px"}} />
-                </div>
                 </div>
                 <div className="row" style={{ height:"100px" }}>
-                <div className="col-md-1" />
-                <div className="col-md-10"><hr/></div>
-                <div className="col-md-1" />
+                    <div className="col-md-1" />
+                    <div className="col-md-10"><hr/></div>
+                    <div className="col-md-1" />
                 </div>
 
                 {/* 추가요청 게시판 */}
                 <div className="row" id={styles.wrapper}>
-                <div className="row">
-                    <div className="col-sm-4 col-md-3" />
-                    <div className="col-sm-5 col-md-6 " >
-                        <FontAwesomeIcon icon={faMoneyCheck} size={"2x"} className="fa"/>
-                        <h3 className="reqlist" onClick={()=>window.location.href='/list'}>요청 게시판</h3>
-                    </div>
-                    <div className="col-sm-3 col-md-2 mt-3">
-                        <div className="row">
-                        <Button className ="col-sm-12 col-md-5 text-center "onClick={()=>window.location.href='/music/write'}>글쓰기</Button>
-                        <div className='col-md-2'>/</div>
-                        <Button className ="col-sm-12 col-md-5 text-center" onClick={()=>window.location.href='/music/list'}>더보기</Button>
+                    <div className="row">
+                        <div className="col-sm-4 col-md-3" />
+                        <div className="col-sm-5 col-md-6">
+                            <FontAwesomeIcon icon={faMoneyCheck} size={"2x"} className="fa"/>
+                            <h3 className="reqlist" onClick={()=>window.location.href='/list'}>요청 게시판</h3>
                         </div>
+                        <div className="col-sm-3 col-md-2 mt-3">
+                            <div className="row">
+                                <Button className ="col-sm-12 col-md-5 text-center "onClick={()=>window.location.href='/music/write'}>글쓰기</Button>
+                                <div className='col-md-2'>/</div>
+                                <Button className ="col-sm-12 col-md-5 text-center" onClick={()=>window.location.href='/music/list'}>더보기</Button>
+                            </div>
+                        </div>
+                        <div className="col-sm-1 col-md-1" />
                     </div>
-                    <div className="col-sm-1 col-md-1"> </div>
-                </div>
-                <div className="row" >
-                    <div className="col-md-1" />
-                    <div className="col-md-3 mt-4">
-                    <Summary setClickvalue={setClickvalue}/>
+                    <div className="row">
+                        <div className="col-md-1" />
+                        <div className="col-md-3 mt-4">
+                            <Summary setClickvalue={setClickvalue}/>
+                        </div>
+                        <div className="col-md-7 mt-3" id={styles.subCommunity}>
+                            <p> {clickvalue} </p>
+                        </div>
+                        <div className="col-md-1" />
                     </div>
-                    
-                    <div className="col-md-7 mt-3" id={styles.subCommunity}>
-                    <p> {clickvalue} </p>
-                    </div>
-
-                    <div className="col-md-1" />
-                </div>
-                <div className="row" style={{ height:"100px"}} />
+                    <div className="row" style={{ height:"120px"}} />
                 </div>
             </div>
         </Container>
